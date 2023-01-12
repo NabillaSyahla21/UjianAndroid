7449d6312061d21102356993711d312403440b32
Nabila Puspita Syahlarani <nabilalacy61@gmail.com>
2023-01-12 14:09:23 +0700

    Tambah Umur

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -19,15 +19,19 @@ public class ListActivity extends AppCompatActivity {
 
         ListView lvNama = (ListView) findViewById(R.id.lvNama);
 
-        ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
+                ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
+
 
         if(daftar_nama.isEmpty()){
             daftar_nama.add("data masih kosong");
+
         }
 
         ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
 
+
         lvNama.setAdapter(ad_nama);
+
 
     }
 }
